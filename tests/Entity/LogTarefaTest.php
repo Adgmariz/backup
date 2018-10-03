d7c874d629cc3ed80fd87daad404d2a6daefcc66
Adgmariz <adgmariz@gmail.com>
2018-10-03 17:10:46 -0300

    logtarefa test 1

---
+++ tests/Entity/LogTarefaTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Tests\Entity;
+
+use App\Entity\LogTarefa;
+use PHPUnit\Framework\TestCase;
+
+class LogTarefaTest extends TestCase{
+
+    private $logTarefa;
+    public function getLogTarefa()
+    {
+        return $this->logTarefa;
+    }
+    public function setLogTarefa($logTarefa)
+    {
+        $this->logTarefa = $logTarefa;
+
+        return $this;
+    }
+
+    /**
+     * @before
+     */
+    public function setUp(){
+        $this->setLogTarefa(new Tarefa());
+        $this->getLogTarefa()->setId(1);
+        $this->getLogTarefa()->setAgendamento(1);
+        $this->getLogTarefa()->setTarefa(1);
+        $this->getLogTarefa()->setLog('log teste');
+    }
+
+    public function testAttr(){
+        $this->assertClassHasAttribute('id', LogTarefa::class);
+        $this->assertClassHasAttribute('agendamento', LogTarefa::class);
+        $this->assertClassHasAttribute('tarefa', LogTarefa::class);
+        $this->assertClassHasAttribute('log', LogTarefa::class);
+    }
+
+}
