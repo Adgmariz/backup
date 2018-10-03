1f9e6fcbccb8b26e2435e879f545fb43c61c7c1b
Adgmariz <adgmariz@gmail.com>
2018-10-03 18:09:13 -0300

    aula

--- tests/Controller/FrontControllerTest.php
+++ tests/Controller/FrontControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FrontControllerTest extends WebTestCase{
 
-    public function testAll(){
+    public function testAllUrls(){
         $urls = ['/', '/dashboard', '/login', '/logout', '/listaragendamentos', '/editaragendamentos/1', '/processaeditagend', '/excluiragendamento/1',
                 '/processacriaragend', '/listartarefas', '/criartarefa', '/processacriartarefa', '/executartarefa/1','/editartarefas/1', '/processaeditatarefa', 
                 '/excluirtarefa/1', '/ajax/executar/1', 'processaagendamentos' ];
@@ -14,13 +14,8 @@ class FrontControllerTest extends WebTestCase{
 
         foreach ($urls as $url) {
             $client->request('GET', $url);
-            var_dump($url);
             $this->assertContains($client->getResponse()->getStatusCode(), [200, 302, 500]);
-            // $this->assertEquals(200, $client->getResponse()->getStatusCode());
         }
-
-        // $client->request('GET', '/');
-        // $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
 }
---
+++ tests/Controller/UsuarioControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Tests\Entity;
+
+use App\Controller\UsuarioController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UsuarioControllerTest extends WebTestCase{
+
+    public function testAllUrls(){
+        $urls = ['/usuario'];
+        $client = static::createClient();
+
+        foreach ($urls as $url) {
+            $client->request('GET', $url);
+            // $this->assertEquals(200, $client->getResponse()->getStatusCode());
+         }
+         var_dump($client->getResponse());
+    }
+
+}
